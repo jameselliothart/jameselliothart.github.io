1d0e9629087a57889a938242732b57e7f6b3b432
James Hart <james.elliot.hart@gmail.com>
2019-10-23 12:10:22 -0500

    personalize site links

--- pelicanconf.py
+++ pelicanconf.py
@@ -22,12 +22,11 @@ AUTHOR_FEED_RSS = None
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+         ('Jinja2', 'http://jinja.pocoo.org/'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('LinkedIn', 'https://www.linkedin.com/in/james-hart-3543664b/'),
+          ('GitHub', 'https://github.com/jameselliothart/'),)
 
 DEFAULT_PAGINATION = 10
 
